4aec302fc5ca1a17a87dfa42b3ec56868f11e87f
Mohammad hussain <eftikharehussain@gmail.com>
2026-03-15 20:55:17 +0430

    Create B. NIT Destroys the Universe.py
    this is my solution for this code forces problem with python

---
+++ B. NIT Destroys the Universe.py
@@ -0,0 +1,21 @@
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    segments = 0
+    i = 0
+
+    while i < n:
+        if a[i] != 0:
+            segments += 1
+            while i < n and a[i] != 0:
+                i += 1
+        else:
+            i += 1
+
+    if segments == 0:
+        print(0)
+    elif segments == 1:
+        print(1)
+    else:
+        print(2)
