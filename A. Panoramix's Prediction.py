9bf3f7907b1e363b47498a57838e90f780291101
Mohammad hussain <eftikharehussain@gmail.com>
2026-03-15 20:53:48 +0430

    Create A. Panoramix's Prediction.py
    this is my solution for this code forces problem with python

---
+++ A. Panoramix's Prediction.py
@@ -0,0 +1,18 @@
+a, b = map(int, input().split())
+lst = []
+for num in range(2,51):
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                break
+        else:
+            lst.append(num)
+if b not in lst:
+    print("NO")
+else:
+    for i in range(len(lst)-1):
+        if lst[i] == a:
+                if lst[i+1] == b:
+                    print("YES")
+                else:
+                    print("NO")
